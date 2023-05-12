3197ea25575e6b186defe8bd62993c49edc002e0
NicolasFCruvinel <nicolasfcmsouza@gmail.com>
2023-05-12 17:40:42 -0300

    retirando erro de página surgindo

--- js/error_cadastro.js
+++ js/error_cadastro.js
@@ -5,11 +5,13 @@ function cadastro(){
 
     let id_unidade = "u" + unidade_cadastrada;
 
-    let status = document.getElementById(id_unidade);
-    if (status.classList.contains('un__item-status status-disponivel')) {
+    let status = document.getElementById(id_unidade); //Colocar aspas em id_unidade
+    if (status.classList.contains('un__item-status status-disponivel')) {  //verificar condição
         window.location.href = "/alertas/cadastro_sucesso.html";
     } else {
         window.location.href = "/alertas/cadastro_falha.html";
 }
 
 }
+// Dica:
+// pesquise sobre o comando console.log para ir visualizando oq está fazendo
